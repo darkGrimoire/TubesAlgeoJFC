d24b50a06a1bea0e8462d8debc0f274d16981528
specialOne16 <jundi2303@gmail.com>
2019-09-15 23:27:32 +0700

    Update driver.java

--- driver.java
+++ driver.java
@@ -1,5 +1,5 @@
-package MatriksDriver;
-import MatriksLib.*;
+package TubesAlgeoJFC;
+import TubesAlgeoJFC.MATRIKS;
 import java.io.*;
 
 class Driver 
@@ -7,18 +7,41 @@ class Driver
     public static void main(String args[]) 
     { 
         MATRIKS M1=new MATRIKS(101, 101);
-        // M1.SetMaksNeffKolom(10);
-        // M1.SetMaksNeffBaris(10);
-        // for(int i=1; i<=M1.GetMaksNeffBaris(); i++){
-        //     for(int j=1; j<=M1.GetMaksNeffKolom(); j++){
-        //         M1.SetNilai(i,j,0);
-        //     }
-        // }
+         M1.SetMaksNeffKolom(4);
+         M1.SetMaksNeffBaris(3);
+
+         M1.SetNilai(1,1,2);
+         M1.SetNilai(1,2,3);
+         M1.SetNilai(1,3,4);
+         M1.SetNilai(1,4,6);
+         M1.SetNilai(2,1,9);
+         M1.SetNilai(2,2,1);
+         M1.SetNilai(2,3,2);
+         M1.SetNilai(2,4,4);
+         M1.SetNilai(3,1,2);
+         M1.SetNilai(3,2,6);
+         M1.SetNilai(3,3,2);
+         M1.SetNilai(3,4,8);
+        //  for(int i=1; 5<=M1.GetMaksNeffBaris(); i++){
+        //      for(int j=1; j<=M1.GetMaksNeffKolom(); j++){
+        //          M1.SetNilai(i,j,3*i+2*j);
+        //      }
+        //  }
         // M1.BacaMatriks();
         // System.out.println();
         // M1.TulisMatriks();
         // MATRIKS M2 = new MATRIKS(101,101);
-        M1.BacaFileMatriks();
         M1.TulisMatriks();
+        System.out.println();
+
+        M1.MetodeEliminasiGauss();
+        M1.TulisMatriks();
+        M1.TulisHasilGauss();
+        System.out.println();
+
+        M1.MetodeEliminasiJordan();
+        M1.TulisMatriks();
+        M1.TulisHasilJordan();
+        System.out.println();
     } 
 } 
