81acc18103f6e0f88fe9ece2579eb6277aefd053
specialOne16 <jundi2303@gmail.com>
2019-09-16 23:04:48 +0700

    Update driver.java

--- driver.java
+++ driver.java
@@ -8,20 +8,23 @@ class Driver
     { 
         MATRIKS M1=new MATRIKS(101, 101);
          M1.SetMaksNeffKolom(4);
-         M1.SetMaksNeffBaris(3);
+         M1.SetMaksNeffBaris(2);
 
-         M1.SetNilai(1,1,2);
-         M1.SetNilai(1,2,3);
-         M1.SetNilai(1,3,4);
-         M1.SetNilai(1,4,6);
-         M1.SetNilai(2,1,9);
-         M1.SetNilai(2,2,1);
-         M1.SetNilai(2,3,2);
-         M1.SetNilai(2,4,4);
-         M1.SetNilai(3,1,2);
-         M1.SetNilai(3,2,6);
-         M1.SetNilai(3,3,2);
-         M1.SetNilai(3,4,8);
+         M1.SetNilai(2,1,2);
+         M1.SetNilai(2,2,3);
+         M1.SetNilai(2,3,0);
+         M1.SetNilai(2,4,2);
+         M1.SetNilai(2,5,1);
+         M1.SetNilai(1,1,1);
+         M1.SetNilai(1,2,1);
+         M1.SetNilai(1,3,2);
+         M1.SetNilai(1,4,1);
+         M1.SetNilai(1,5,5);
+         M1.SetNilai(3,1,1);
+         M1.SetNilai(3,2,2);
+         M1.SetNilai(3,3,4);
+         M1.SetNilai(3,4,3);
+         M1.SetNilai(3,5,1);
         //  for(int i=1; 5<=M1.GetMaksNeffBaris(); i++){
         //      for(int j=1; j<=M1.GetMaksNeffKolom(); j++){
         //          M1.SetNilai(i,j,3*i+2*j);
@@ -36,12 +39,12 @@ class Driver
 
         M1.MetodeEliminasiGauss();
         M1.TulisMatriks();
-        M1.TulisHasilGauss();
+        //M1.TulisHasilGauss();
         System.out.println();
 
         M1.MetodeEliminasiJordan();
         M1.TulisMatriks();
         M1.TulisHasilJordan();
-        System.out.println();
+        //System.out.println();
     } 
 } 
